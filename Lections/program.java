c4c1443bc80219429d6b84a1751635a8d985fa36
Anna <tolstovaaniuta@yandex.ru>
2022-12-14 13:58:28 +0300

    lections

---
+++ Lections/program.java
@@ -0,0 +1,41 @@
+// public class program {
+//     public static void main(String[] args) {
+//         String s = "srtdflkvn";
+//         System.out.println(s);
+//         s = s + "23";
+//         System.out.println(s);
+//     }
+// }
+
+/**
+ * Innerprogram
+ */
+// public class program {
+//     public static void main(String[] args) {
+//         char ch = 115;
+//         System.out.println(ch);
+//     }
+// }
+
+/**
+ * program
+ */
+// public class program {
+
+//     public static void main(String[] args) {
+//         boolean f = true ^ false;
+//         System.out.println(f);
+
+//     }
+// }
+
+/**
+ * program
+ */
+public class program {
+
+    public static void main(String[] args) {
+        String msg = "Hello, world!";
+        System.out.println(msg);
+    }
+}
