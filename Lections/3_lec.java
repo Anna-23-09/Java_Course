062d162f22191f2cd79f48196b71aa0a64dee7e8
Anna <tolstovaaniuta@yandex.ru>
2022-12-14 13:58:44 +0300

    lections

---
+++ Lections/3_lec.java
@@ -0,0 +1,10 @@
+public class 3_lec {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(2809);
+
+        for (Object o: list) {
+            System.out.println(o);
+        }
+    }
+}
