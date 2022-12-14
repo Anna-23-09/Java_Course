a832bb52cc0fb5929c2424b161fc86e4cef24b56
Anna <tolstovaaniuta@yandex.ru>
2022-12-14 15:29:15 +0300

    ispravlenie

--- HW/HW_1/task3.java
+++ HW/HW_1/task3.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class task3 {
     
     public static void main(String[] args) {
@@ -6,16 +8,16 @@ public class task3 {
         double res;
         char act;
 
-        Scanner scan = new Scanner(System.in);
-        System.out.print("Введите число a: ");
-        a = scan.nextDouble();
-        
-        System.out.print("Введите действие: ");
-        act = scan.next().charAt(0);
-        
-        System.out.print("Введите число b: ");
-        b = scan.nextDouble();
-
+        try (Scanner scan = new Scanner(System.in)) {
+         System.out.print("Введите число a: ");
+           a = scan.nextDouble();
+           
+           System.out.print("Введите действие: ");
+           act = scan.next().charAt(0);
+           
+           System.out.print("Введите число b: ");
+           b = scan.nextDouble();
+      }
         switch(act) {
             case '+': res = a + b;
                break;
