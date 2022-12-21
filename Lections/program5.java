1f172b0d567d0b8c2bb5dbdcaa0b49ac086646e7
Anna <tolstovaaniuta@yandex.ru>
2022-12-21 15:55:43 +0300

    5 lection

---
+++ Lections/program5.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class program5 {
+    public static void main(String[] args) {
+        
+    }
+
+    public class Ex003 HashMapBoost {
+        public static void main (String[] args) {
+            Map<Integer, String> map1 = new HashMap<>();
+            Map<Integer, String> map2 = new HashMap<>(9);
+            Map<Integer, String> map3 = new HashMap<>(9, 1.0f);
+        }
+    }
+}
