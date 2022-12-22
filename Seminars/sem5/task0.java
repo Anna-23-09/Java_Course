bf9ed28978eca3f94031a07ad9b9296b927be5c0
Anna <tolstovaaniuta@yandex.ru>
2022-12-22 14:52:23 +0300

    0 task

--- Seminars/sem5/task0.java
+++ Seminars/sem5/task0.java
@@ -3,15 +3,18 @@ package sem5;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class task0 {
     public static void main(String[] args) {
+
+        Fio fio = new Fio("Fam", "Nam", "SoNam");
         
         HashMap<Integer, String> map = new HashMap<>();
 
         map.clear();
-        map.put(1, "One");
+        map.put(1, new Fio("Fam", "Nam", "SoNam"));
         map.put(2, "Dva");
         map.putIfAbsent(4, "Chetity");
         map.put(15, "P");
@@ -22,13 +25,12 @@ public class task0 {
         String vc = map.get(1);
         map.equals(2);
         map.containsValue("One");
-        map.conteinsKey(34);
+        map.containsKey(34);
         Set<Integer> a = map.keySet();
-        Collection<String> v = map.Values();
+        Collection<String> v = map.values();
         map.replace(1, "Tri");
-        map.getOfDefault(key: 2, defaultValue: "Def");
         map.isEmpty();
-        map.remove(key:2);
+        map.remove(2);
         
         for (Integer i : map.keySet()) {
             System.out.println(map.get(i));
@@ -46,7 +48,18 @@ public class task0 {
         treeMap.put(3, "3");
         treeMap.put(4, "4");
 
-        treeMap.
+    }
+}
 
+
+class Fio {
+    String f;
+    String n;
+    String n2;
+
+    public Fio (String f, String n, String n2) {
+        this.f = f;
+        this.n = n;
+        this.n2 = n2;
     }
 }
