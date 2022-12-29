e65d4964ba8b898693c8b3ce3b3962fb42286eb0
Anna <tolstovaaniuta@yandex.ru>
2022-12-29 13:14:16 +0300

    sem

--- Seminars/sem_6/main.java
+++ Seminars/sem_6/main.java
@@ -3,7 +3,7 @@ package sem_6;
 public class main {
     public static void main(String[] args) {
         MySet my = new MySet();
-        my.add(new Person() )
+        my.add(new Person() );
 
     }
 }
